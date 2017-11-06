ec628409aaf27153915a48425651f65d0928800a
Samantha Wang <samanthawang318@gmail.com>
2017-11-05 22:37:44 -0800

    remove function

--- modules/coolNumber.hs
+++ modules/coolNumber.hs
@@ -4,8 +4,5 @@ import Data.Char
 digitSum :: Int -> Int
 digitSum = sum . map digitToInt . show
 
-firstTo40 :: Maybe Int
-firstTo40 = find(\x -> digitSum x == 40)[1 ..]
-
 firstTo :: Int -> Maybe Int
 firstTo n = find(\x -> digitSum x == n)[1 ..]
