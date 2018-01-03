d45d91b7bfc1e79e2a6e013689f1f31c49439317
Samantha Wang <samanthawang318@gmail.com>
2018-01-02 21:17:04 -0800

    finishing up chapter7

--- type/tree.hs
+++ type/tree.hs
@@ -16,3 +16,7 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a = treeElem x left
     | x > a = treeElem x right
+
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
