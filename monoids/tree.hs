0ecb56bdbc0b36fb0cfac612c1f2df98f30621d2
Samantha Wang <samanthawang318@gmail.com>
2018-02-19 21:38:24 -0800

    monads

---
+++ monoids/tree.hs
@@ -0,0 +1,9 @@
+import Data.Monoid
+import qualified Data.Foldable as F
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
